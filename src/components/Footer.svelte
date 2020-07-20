6d4e29383201b98d23f9542778ed8682b630b71a
JuanGalvis <juangalvis.sh@gmail.com>
2020-07-20 17:09:01 -0500

    Estructura casi terminada

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+  let Year = new Date().getFullYear();
+</script>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      <h2>© {Year} PUGSTAGRAM FROM PLATZI</h2>
+    </div>
+  </div>
+</div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,10 +1,37 @@
+<script>
+  import { onMount } from "svelte";
+
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+
+  let data = {};
+  const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
+</script>
+
 <style>
   :global(body) {
-    background-color: #676778;
-    color: white;
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato";
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
   }
 </style>
 
-<div class="main">
-  <h1>Hola Mundo</h1>
-</div>
+<Header />
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
